ed346f270d16d806793ed6676b05565870da9e92
dhanume221 <dhanushmr55@gmail.com>
2025-12-11 23:54:58 +0400

    new

--- backendserver/server.js
+++ backendserver/server.js
@@ -13,7 +13,22 @@ const swaggerDoc = JSON.parse(readFileSync(new URL('./swagger.json', import.meta
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    process.env.FRONTEND_URL
+];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+}));
+
 app.use(express.json());
 
 connectDB();
